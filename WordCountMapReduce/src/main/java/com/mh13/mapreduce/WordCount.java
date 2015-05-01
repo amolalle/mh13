cabf41989fc3436122dec73b4c3a44424f5feb2e
Amol <Amol.Alle@firstdata.com>
2015-05-01 00:39:52 -0400

    A simple map reduce(MR2) job.

---
+++ WordCountMapReduce/src/main/java/com/mh13/mapreduce/WordCount.java
@@ -0,0 +1,59 @@
+package com.mh13.mapreduce;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount {
+
+	public static void main(String[] args) throws IOException,
+			InterruptedException, ClassNotFoundException {
+
+		Path inputPath = new Path(args[0]);
+		Path outputDir = new Path(args[1]);
+
+		// Create configuration
+		Configuration conf = new Configuration(true);
+
+		// Create job
+		Job job = new Job(conf, "WordCount");
+		job.setJarByClass(WordCountMapper.class);
+
+		// Setup MapReduce
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		job.setNumReduceTasks(1);
+
+		// Specify key / value
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// Input
+		FileInputFormat.addInputPath(job, inputPath);
+		job.setInputFormatClass(TextInputFormat.class);
+
+		// Output
+		FileOutputFormat.setOutputPath(job, outputDir);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		// Delete output if exists
+		FileSystem hdfs = FileSystem.get(conf);
+		if (hdfs.exists(outputDir))
+			hdfs.delete(outputDir, true);
+
+		// Execute job
+		int code = job.waitForCompletion(true) ? 0 : 1;
+		System.exit(code);
+
+	}
+
+}
